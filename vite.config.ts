51608213bfc414629b2a1755c90c5c8dbe8bf79f
Mason <mtmitchel@gmail.com>
2025-06-27 00:05:38 -0400

    Fix base path in vite.config.ts to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/clarity-and-confidence/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/clarityandconfidence/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
